9c9b3355aefd55dae84990faf0e00ccc75f59d21
ericauv <eriauv17@gmail.com>
2019-12-22 22:48:37 -0500

    add images.js for importing all images

---
+++ client/images.js
@@ -0,0 +1,8 @@
+const images = {};
+
+function importAll (r) {
+  r.keys().forEach(key => images[key] = r(key));
+}
+
+importAll(require.context('../assets/', true, /\.(png|svg|jpe?g|gif)$/));
+export default Object.values(images).map(mod=>`./${mod.default}`);
