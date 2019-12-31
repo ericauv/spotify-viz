7cb2c6652b6441f15ef5caa8fa739cc8e942b608
ericauv <eriauv17@gmail.com>
2019-12-31 17:31:36 -0500

    ready for nye

--- client/template.js
+++ client/template.js
@@ -1,8 +1,15 @@
 import Visualizer from './classes/visualizer'
 import {interpolateNumber} from 'd3-interpolate'
+import {differenceInHours, differenceInMinutes, differenceInSeconds, differenceInMilliseconds,formatDistanceToNow, addMinutes, startOfMinute} from 'date-fns'
 import repeatTextVertically from './visualizations/repeatTextVertically'
 import repeatText from './visualizations/repeatText'
 import images from './images'
+
+
+const newYear = new Date('January 1, 2020');
+// const newYear = new Date('December 31, 2019 17:28');
+// const newYearEndVisuals = new Date('December 31, 2019 17:29');
+const newYearEndVisuals = new Date('January 1, 2020 00:01:30');
 export default class Template extends Visualizer {
   constructor () {
     super({ volumeSmoothing: 60})
@@ -10,33 +17,39 @@ export default class Template extends Visualizer {
       sync:{
         segment:{
           random:[],
+          randomSetsOfFour:[],
           value:0,
           max:16
         },
         tatum:{
           random:[],
+          randomSetsOfFour:[],
           value:0,
           max:8
         },
         beat:{
           random:[],
+          randomSetsOfFour:[],
           value:0,
-          max:4
+          max:4,
+          secondsLeft:60
         },
         bar:{
           random:[],
+          randomSetsOfFour:[],
           value:0,
           max:4
         },
         section:{
           random:[],
+          randomSetsOfFour:[],
           value:0,
           max:4
         }
       },
       timbreRectangles:{
         draw:true,
-        number:3,
+        number:5,
         depth:30,
         spacing:40
       },
@@ -83,6 +96,9 @@ export default class Template extends Visualizer {
   this.sync.on('beat', beat => {
 
     this.handleStateUpdateForSync('beat');
+    this.state.sync.beat.secondsLeft = differenceInSeconds(newYear, Date.now())-(differenceInMinutes(newYear,Date.now())*60);
+
+
 
     // store volume to state
     this.state.volume = this.sync.volume;
@@ -94,7 +110,6 @@ export default class Template extends Visualizer {
   })
 
   this.sync.on('bar', bar => {
-    this.generateRandomSetsOfFour();
 
       this.handleStateUpdateForSync('bar');
 
@@ -111,12 +126,12 @@ export default class Template extends Visualizer {
     })
   }
 
-  generateRandomSetsOfFour = () => {
+  generateRandomSetsOfFour = (syncInterval='segment') => {
      // create random Sets of 4 numbers
      for(let counter=0;counter<=200;counter++){
-      this.state.randomSetsOfFour[counter]=[];
+      this.state.sync[syncInterval].randomSetsOfFour[counter]=[];
       for(let index=0;index<4;index++){
-        this.state.randomSetsOfFour[counter][index] = Math.random();
+        this.state.sync[syncInterval].randomSetsOfFour[counter][index] = Math.random();
       }
     }
   }
@@ -136,6 +151,8 @@ export default class Template extends Visualizer {
   handleStateUpdateForSync = (syncIntervalName='segment') => {
     this.incrementSync(syncIntervalName);
     this.generateRandomNumberForSync(syncIntervalName);
+    this.generateRandomSetsOfFour(syncIntervalName);
+
   }
 
   generateRandomNumberForSync = (syncIntervalName='segment') => {
@@ -152,6 +169,12 @@ export default class Template extends Visualizer {
 
     // clear canvas from previous paint
     ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle =' white';
+    const hoursLeft = differenceInHours(newYear, Date.now());
+    const minutesLeft = differenceInMinutes(newYear, Date.now())-(hoursLeft*60);
+    const secondsLeft = differenceInSeconds(newYear, Date.now())-(differenceInMinutes(newYear,Date.now())*60);
+    const millisecondsLeft = differenceInMilliseconds(newYear, Date.now())-(differenceInSeconds(newYear,Date.now())*1000);
+
 
     // Background
     const gradient = ctx.createLinearGradient(0,0,width+width/4,height+height/4);
@@ -163,54 +186,129 @@ export default class Template extends Visualizer {
     ctx.fillRect(0, 0, width, height);
 
 
-    // photo background
-    // ctx.drawImage(this.state.backgroundImage, width/27 * this.state.image, height/27*this.state.image, 300,300);
-    // ctx.drawImage(this.state.backgroundImage, width-width/27 * this.state.image, height-height/27*this.state.image, 300,300);
-    // drawRandomImages(ctx,this.state.randomSetsOfFour,0,width,height,2,images);
 
-    // random rectangles
-    drawRandomRectangles(ctx,this.state.randomSetsOfFour,0,width,height,2,9);
-    // drawRandomRectangles(ctx,this.state.randomSetsOfFour,0,width,height,1,1);
+
 
     // set range for fontSize
-    const textSizeByVolume = interpolateNumber(44,100) ;
-    ctx.font=`${textSizeByVolume((this.state.grow ? 1.2 : 0.8)*this.sync.volume )}px Arial`;
-    ctx.font = '900 Arial'
+    // const textSizeByVolume = interpolateNumber(44,100) ;
+    // ctx.font=`${textSizeByVolume((this.state.grow ? 1.2 : 0.8)*this.sync.volume )}px Arial`;
+    // ctx.font = '900 Arial'
     ctx.lineWidth= 2.2;
-    ctx.textAlign ='center';
+    // ctx.textAlign ='center';
 
-    ctx.fillStyle= 'white';
+    // ctx.fillStyle= 'white';
 
     // timbre rectangles
-    if(this.state.timbreRectangles.draw){
-      for(let i = 0; i<=Math.min(11,this.state.timbreRectangles.number-1);i++){
-        drawCenterEdgeRectangles(ctx, this.sync.segment.timbre[i]/95*(this.state.grow ? 1.1 : 0.6) * this.sync.volume, false, 'red', width/2, height/2, this.state.timbreRectangles.depth, width,height, i*(this.state.timbreRectangles.depth +this.state.timbreRectangles.spacing));
+    if(minutesLeft%10 === 0 && (minutesLeft+hoursLeft) > 0){
+      console.log(hoursLeft,minutesLeft, minutesLeft%10);
+      let edgeRectangleCount = Math.min(11,this.state.timbreRectangles.number-1);
+      if(this.state.sync.beat.secondsLeft<5){
+        edgeRectangleCount = 4 - this.state.sync.beat.value;
       }
+        for(let i = 0; i<=edgeRectangleCount;i++){
+          // random fill rectangles
+          if(secondsLeft <=20){
+            drawCenterEdgeRectangles(ctx, this.sync.segment.timbre[i]/95*(this.state.grow ? 1.1 : 0.6) * this.sync.volume, true, `rgba(${Math.random()*255},${Math.random()*255},${Math.random()*255},${Math.random()})`, width/2, height/2, this.state.timbreRectangles.depth, width,height, i*(this.state.timbreRectangles.depth +this.state.timbreRectangles.spacing));
+          }
+          // stroke rectangles
+          drawCenterEdgeRectangles(ctx, this.sync.segment.timbre[i]/95*(this.state.grow ? 1.1 : 0.6) * this.sync.volume, false, 'red', width/2, height/2, this.state.timbreRectangles.depth, width,height, i*(this.state.timbreRectangles.depth +this.state.timbreRectangles.spacing));
+        }
     }
 
-    // repeated Text
-    ctx.save();
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = 'black';
-    ctx.font = '92px Arial';
 
-    // Song Name
-    ctx.textAlign = 'center';
-    ctx.fillStyle = 'black';
-    repeatTextVertically(ctx,this.sync.state.currentlyPlaying.name, 'fill', Math.round(this.state.randomSetsOfFour[0][1]*9)*width/9, height/2,50,false,this.state.sync.beat.value,2*width/3);
 
-    // Artist Name
-    ctx.textAlign = 'left'
-    repeatTextVertically(ctx,this.sync.state.currentlyPlaying.artists[0].name, 'fill', Math.round(this.state.randomSetsOfFour[0][0]*9)*width/9, height/9,50,false,3,2*width/3);
 
-    // repeated ninths
-    // ctx.textAlign = 'right'
-    // for(let ninthCount=8;ninthCount>=1;ninthCount--){
-    //   repeatedSyncTextAtNinthsOfWidth(ctx,width,height,'//','stroke',1);
 
-    // }
-    ctx.restore();
-  }
+
+
+      if(Date.now() < newYear){
+        // Last minute
+        if(Date.now() < newYear && hoursLeft < 1 && minutesLeft<=1){
+          const numberOfRects = Math.round((60-this.state.sync.beat.secondsLeft)/3);
+          drawRandomRectangles(ctx,this.state.sync.bar.randomSetsOfFour,0,width,height,numberOfRects,9);
+        }
+        ctx.fillStyle ='white';
+        // Countdown text
+        ctx.save();
+        ctx.font = '18px Arial';
+        ctx.fillStyle ='white';
+        ctx.textAlign = 'right';
+        ctx.textBaseline = 'bottom';
+        ctx.fillText(`-[${padWithZeroes(hoursLeft,2)}:${padWithZeroes(minutesLeft,2)}:${padWithZeroes(secondsLeft,2)}:${padWithZeroes(millisecondsLeft,3)}]`,width/81*80,height/81*79);
+
+        ctx.textAlign = 'right';
+        ctx.textBaseline = 'top';
+        ctx.font = '12px Arial';
+        ctx.fillText(formatDistanceToNow(newYear),width/81*80,height/81*79);
+
+        ctx.restore();
+
+      // Last 10 seconds
+      if(hoursLeft<1 && secondsLeft<=60){
+        ctx.save();
+
+        ctx.textAlign = 'center';
+
+        ctx.textBaseline = 'middle';
+
+        const smallFontSize = Math.round((60-this.state.sync.beat.secondsLeft)/2) * 12;
+        const largeFontSize = Math.round((60-this.state.sync.beat.secondsLeft)/2) *15;
+        const fontSize = this.state.sync.beat.value %2 ? largeFontSize : smallFontSize;
+        ctx.font = `${fontSize}px Arial`;
+        ctx.fillText(secondsLeft,width/2,height/2);
+        ctx.strokeText(secondsLeft,width/2,height/2);
+        ctx.restore();
+      }
+      // }
+    }
+
+    // New Year
+    if(Date.now() >= newYear && Date.now() < newYearEndVisuals){
+      // Last minute
+      drawRandomRectangles(ctx,this.state.sync.bar.randomSetsOfFour,0,width,height,30,9);
+      for(let i =0;i<=(Math.round(this.sync.beat.progress*9));i++){
+        ctx.save();
+        ctx.textAlign ='left';
+        ctx.textBaseline = 'top';
+        ctx.font = '96px Arial';
+        for(let j=0;j<=(Math.round(this.sync.beat.progress*9));j++){
+          if(this.sync.bar.progress <= 0.7){
+
+            ctx.fillStyle = 'black';
+          }else{
+            ctx.fillStyle ='white';
+          }
+          ctx.font = '96px Arial black';
+
+          ctx.fillText('2020',width/9*i,height/9*j,width/9);
+        }
+        ctx.restore();
+      }
+    }
+    // Song and Artist
+    if(!(Date.now() < newYearEndVisuals && Date.now() >= newYear)){
+
+     ctx.save();
+     ctx.textAlign = 'right';
+
+     ctx.textBaseline = 'bottom';
+
+     ctx.font = '12px Arial';
+     ctx.fillStyle = 'white';
+     const songPosition = Date.now() > newYear ? 78.5 : 75;
+     const artistPosition = Date.now() > newYear ? 80 : 76.5;
+     ctx.fillText(this.sync.state.currentlyPlaying.name,width/81*80,height/81*songPosition);
+     ctx.fillText(this.sync.state.currentlyPlaying.artists[0].name,width/81*80,height/81*artistPosition);
+     ctx.restore();
+   }
+    }
+}
+
+
+
+const padWithZeroes = (number, desiredLength=2)=>{
+  const zeroesAndNumber = '000' + number;
+  return('000'+number).substr(-(desiredLength));
 }
 
 const repeatedSyncTextAtNinthsOfWidth = (ctx, width=1000, height=1000, textContent='', fillOrStroke='fill', syncValue=1) => {
@@ -228,27 +326,29 @@ const drawMultipleCenterEdgeRectangles = (numberOfRectangles) => {
 
 }
 
-const drawRandomImages = (ctx, randomSetsOfFour=[[]],randomSeedAddition, width=1000,height=1000, numberOfImages=1, images=[]) => {
+const drawRandomImages = (ctx, randomSetsOfFour=[[]],randomSeedAddition, width=1000,height=1000, numberOfImages=1, gridDivision =9,images=[]) => {
   const image = new Image();
   const imageInstanceLimit = Math.min(numberOfImages, randomSetsOfFour.length)
-  for(let imageInstance = 0; imageInstance<imageInstanceLimit;imageInstance++){
+  for(let currentIndex = 0; currentIndex<imageInstanceLimit;currentIndex++){
     // pick a random image from images to draw
-    image.src = images[Math.min(images.length-1,Math.round(randomSetsOfFour[imageInstance][0]*(images.length-1)))];
+    image.src = images[Math.min(images.length-1,Math.round(randomSetsOfFour[currentIndex][0]*(images.length-1)))];
 
-    const randomX = Math.round(Math.round(randomSetsOfFour[imageInstance+randomSeedAddition][0]*9)  * width/9);
-    const randomY = Math.round(Math.round(randomSetsOfFour[imageInstance+randomSeedAddition][1]*9)  * height/9);
-    const randomWidth = Math.round(Math.round(randomSetsOfFour[imageInstance+randomSeedAddition][2]*9+1)  * width/9);
-    const randomHeight = Math.round(Math.round(randomSetsOfFour[imageInstance+randomSeedAddition][3]*9+1)  * height/9);
+    const {randomX,randomY,randomWidth,randomHeight} = randomCoordinatesAndSize(randomSetsOfFour,randomSeedAddition, currentIndex, width,height,gridDivision);
     // draw the image in a random position and size
     ctx.drawImage(image,randomX,randomY,randomWidth,randomHeight);
   }
 }
+
+const randomCoordinatesAndSize = (randomSetsOfFour, randomSeedAddition, currentIndex, width=1000, height=1000, gridDivision=9) => {
+  const randomX = Math.round(Math.min(width-width/gridDivision,Math.round(randomSetsOfFour[currentIndex+randomSeedAddition][0]*gridDivision)  * width/gridDivision));
+  const randomY = Math.round(Math.min(height-height/gridDivision,Math.round(randomSetsOfFour[currentIndex+randomSeedAddition][1]*gridDivision)  * height/gridDivision));
+  const randomWidth = Math.round(Math.round(randomSetsOfFour[currentIndex+randomSeedAddition][2]*gridDivision+1)  * width/gridDivision);
+  const randomHeight = Math.round(Math.round(randomSetsOfFour[currentIndex+randomSeedAddition][3]*gridDivision+1)  * height/gridDivision);
+return {randomX,randomY,randomWidth,randomHeight};
+}
 const drawRandomRectangles = (ctx, randomSetsOfFour=[[]],randomSeedAddition, width=1000,height=1000, numberOfRectangles=1, gridDivision=9) => {
-  for(let rectangleCount = 0; rectangleCount<numberOfRectangles;rectangleCount++){
-    const randomX = Math.round(Math.min(width-width/gridDivision,Math.round(randomSetsOfFour[rectangleCount+randomSeedAddition][0]*gridDivision)  * width/gridDivision));
-    const randomY = Math.round(Math.min(height-height/gridDivision,Math.round(randomSetsOfFour[rectangleCount+randomSeedAddition][1]*gridDivision)  * height/gridDivision));
-    const randomWidth = Math.round(Math.round(randomSetsOfFour[rectangleCount+randomSeedAddition][2]*gridDivision+1)  * width/gridDivision);
-    const randomHeight = Math.round(Math.round(randomSetsOfFour[rectangleCount+randomSeedAddition][3]*gridDivision+1)  * height/gridDivision);
+  for(let currentIndex = 0; currentIndex<numberOfRectangles;currentIndex++){
+    const {randomX,randomY,randomWidth,randomHeight} = randomCoordinatesAndSize(randomSetsOfFour,randomSeedAddition, currentIndex, width,height,gridDivision);
     ctx.save();
     ctx.fillStyle = `rgba(${Math.random()*255},${Math.random()*255},${Math.random()*255},${Math.random()})`;
     ctx.fillRect(randomX,randomY,randomWidth,randomHeight);
